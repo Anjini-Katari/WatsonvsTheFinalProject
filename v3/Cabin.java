15539e4158d38e7bb059624b903dd0949334158f
Anjini Katari <akatari30@stuy.edu>
2022-01-19 13:19:34 -0500

    unfinished v3

---
+++ v3/Cabin.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Cabin {
+
+	protected static String _name;
+	protected static String[] _pronouns;
+	private static int choice;
+	private static boolean gameOver;
+	public static String name = "";
+	private static InputStreamReader isr;
+	private static BufferedReader in;
+	public static String s; 
+
+
+        public Cabin() {
+                gameOver = false;
+                isr = new InputStreamReader( System.in );
+                in = new BufferedReader( isr );
+                choice = 0;
+        }
+
+        public Cabin( String name, String[] pronouns ) {
+		this();
+                _name = name;
+                _pronouns = pronouns;
+		cabin();
+        }
+       	public static void cabin() {
+                s = "\t1: Up the mountain\n";
+                s += "\t2: Down the mountain \n";
+                s += "Selection: ";
+
+                System.out.println(s);
+
+                
+
+                try {
+                        choice  = Integer.parseInt( in.readLine() );
+                }
+
+                catch ( IOException e ) { }
+
+                if (choice == 1) {
+                        s = "Up the mountain we go!";
+                        System.out.println(s);
+                        UpMountain up = new UpMountain(_name, _pronouns);
+
+                }
+                else {
+                        s = "Down, down, down the mountain!";
+                        System.out.println(s);
+                        //down = new DownMountain();
+                }
+
+
+        }
+}
---
+++ v3/UpMountain.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+public class UpMountain {
+	protected String __name;
+	protected String[] _pronouns;
+
+	protected int choice;
+	protected String[] pronouns = new String[3];
+	protected boolean gameOver;
+
+	protected InputStreamReader isr;
+	protected BufferedReader in;
+
+	public UpMountain() {
+		gameOver = false;
+		isr = new InputStreamReader( System.in );
+		in = new BufferedReader( isr );
+		choice = 0;
+
+	}
+
+	public UpMountain( String name, String[] pronouns ) {
+		this();
+        	__name = name;
+		_pronouns = pronouns;
+		Mountain();
+    	}
+
+    // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+    public String getName() { return __name;}
+    public String[] getPronouns() { return _pronouns; }
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	public void Mountain() {
+		String s = "You and your friend ascend up the mountain when you encounter two magical beings: A phoenix and a pixie! \n The phoenix's eyes light up as it telepathically beckons for you to follow it. \n The pixie rolls its eyes, 'Don't listen to the pheonix, follow me!' ";
+		s += "Who will you follow? \n";
+		s += "\t1: The pheonix \n";
+		s += "\t2: The pixie \n";
+		s += "Selection: ";
+		System.out.print( s );
+
+		try {
+			choice  = Integer.parseInt( in.readLine() );
+                }
+
+		catch ( IOException e ) { }
+
+                if (choice == 1) {
+			pheonix();
+                }
+                else {
+			pixie();
+                }
+	}
+
+	public void pixie() {
+		String s = "The pixie giggles and flies off, leaving a trail of pixie dust behind for you to follow. \n Eventually you reach a small witches hut and decide to enter...\n The door creaks open and you enter slowly. As you explore the cabin you encounter a witch! \n";
+		s += "The witch cackles mysteriously, '";
+		s += getName();
+		s += " right? I've heard all about you...' You start to feel a little uneasy and back off slowly \n";
+		s += "The witch cackles again, 'don't worry, I'll make sure everything is as painless as possible \n'";
+		s += "\t1: run \n";
+                s += "\t2: FIGHT \n";
+                s += "Selection: ";
+                System.out.print( s );
+
+                try {
+                        choice  = Integer.parseInt( in.readLine() );
+                }
+
+                catch ( IOException e ) { }
+
+                if (choice == 1) {
+                        run();
+                }
+                else {
+                        fight();
+                }
+	}
+
+	public void run() {
+		String s = "You sprint away quickly, running as fast as you possibly can. Somehow, you end up back at your log cabin, perhaps you should try a different path this time... \n";
+		System.out.print( s );
+		Cabin.cabin();
+	}
+
+	public void fight() {
+
+	}
+	public void pheonix() {
+	}
+
+
+}
