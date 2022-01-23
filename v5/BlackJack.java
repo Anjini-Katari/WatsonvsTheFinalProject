4628f077a1fe90a40065865bd559940f050673bf
rubyfriedman <rfriedman30@stuy.edu>
2022-01-23 16:24:09 -0500

    blackjack changes

--- v5/BlackJack.java
+++ v5/BlackJack.java
@@ -3,10 +3,18 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class BlackJack {
+	protected int choice;
+	protected InputStreamReader isr;
+	protected BufferedReader in;
+	public boolean death;
+
 	public ArrayList deck= new ArrayList<Integer>();
-	public int yourNum = 0;
+	public int YourNum = 0;
 	public int GobNum = 0;
 	public BlackJack() {
+		isr = new InputStreamReader( System.in );
+		in = new BufferedReader( isr );
+		choice = 0;
 		for (int i = 0; i < 4; i++) {
 			for (int j = 1; j <= 13; j++) {
 				deck.add(j);
@@ -16,32 +24,83 @@ public class BlackJack {
 
 	}
 
-	public blackjack(){
-		String s = "The goblin deals you a card...";
+	public boolean getDeath() { return death;}
+	public void blackjack(){
+		System.out.print("Attempt to beat the Goblins by getting a count as close to 21 as possible, without going over 21!");
+		String s = "\n The goblin deals you a card...";
 		System.out.println(s);
 		Humhelper();
 		boolean var = true;
 		while (var) {
-			if (GobNum <= 17) {
-				GobHelper();
-			}
-			else if (GobNum > 21) {
+			if (GobNum > 21) {
 				System.out.println("You win!");
 				var = false;
+				death = false;
+				break;
 			}
-			else if (Gob == 21) {
-				System.out.println("The Goblin has won...");
+			else if (GobNum == 21) {
+				System.out.println("The Goblin has won... death is your fate");
 				var = false;
+				death = true;
+				break;
+			}
+			else if(YourNum == 21) {
+                                System.out.println("You win!");
+				death = false;
+                                var = false;
+				break;
+                        }
+   	                else if (YourNum > 21) {
+                                System.out.println("The Goblin has won... death is your fate");
+				death = true;
+                                var = false;
+				break;
+                        }
+			else {
+                                if (GobNum <= 17) {
+                                        GobHelper();
+                                }
+        	       		System.out.print("\n Would you like to hit or pass? \n");
+
+	        	        s = "\t1: Hit \n";
+	     	          	s += "\t2: Pass \n";
+	        	        s += "Selection: ";
+
+	       		        System.out.println(s);
+
+	          	        try {
+	               			choice  = Integer.parseInt( in.readLine() );
+
+	               		}
+        		        catch ( IOException e ) { }
+
+	                        if (choice == 1) {
+			                Humhelper();
+	  	                }
+		                else {
+					if (YourNum > GobNum) {
+	        	                        System.out.println("You win!");
+						death = false;
+        	        	                var = false;
+                		                break;
+					}
+					else if (GobNum > YourNum) {
+                	                	System.out.println("The Goblin has won... death is your fate");
+						death = true;
+                       		        	var = false;
+                       		        	break;
+					}
+	        	        }
 			}
 		}
 
 
 	}
-	public Humhelper() {
+	public  void Humhelper() {
 		String s = "\n It's a ";
-		int index = Math.random()* deck.size();
+		int index = (int)(Math.random() * deck.size());
 		s += deck.get(index);
-		yourNum += deck.get(index);
+		YourNum += (int)(deck.get(index));
 		deck.remove(index);
 		if (index <= 13) {
 			s += " of spades!";
@@ -60,16 +119,16 @@ public class BlackJack {
 		s+= " points";
 		System.out.print(s);
 	}
-        public Gobhelper() {
-                String s = "\n It's a ";
-                int index = Math.random()* deck.size();
+        public void GobHelper() {
+                String s = "\n The Goblin draws a card, it's a ";
+                int index = (int)(Math.random()* deck.size());
                 s += deck.get(index);
-		GobNum += deck.get(index);
+		GobNum += (int) (deck.get(index));
                 deck.remove(index);
                 if (index <= (deck.size()/4)) {
                         s += " of spades!";
                 }
-                else if (index <= (deck.size()/2) {
+                else if (index <= (deck.size()/2)) {
                         s += " of hearts!";
                 }
                 else if (index <= ((deck.size() * 3) /4)) {
--- v5/DownMountain.java
+++ v5/DownMountain.java
@@ -31,12 +31,22 @@ public class DownMountain {
 	public ArrayList<String> getTreasure() { return _treasure; }
 
     public void Mountain(){
-        String s = "You and your friend descend down the mountain. \n You pass a trio of menacing looking gremlins, but luckily they appear to be preoccupied.";
-	s += "As you continue your decent, the two of you come across a giant pile of ruins. \n Even though the structure is very destroyed, ";
+        String s = "You and your friend descend down the mountain. \n You pass a trio of menacing looking goblins. \n You attempt to walk by them, but one of them blocks your path. \n";
+	s += "'HALT! Whoever dare pass, must first beat us goblins in a deathly battle of BlackJack'\n";
+	System.out.print(s);
+	BlackJack FIGHT = new BlackJack();
+	if (FIGHT.getDeath() == false) {
+		Win();
+	}
+   }
+
+   public void Win(){
+	String s = "A pheonix materializes \n 'Congrats on winning BlackJack, that was a very impressive game' the pheonix chirps \n";
+	s += "The pheonix beckons for your to follow it, and soon you come across a giant pile of ruins. \n Even though the structure is very destroyed, ";
 	s += "you can tell that it was once a very majestic building. \n Your friend appears captivated by a part of the ruin and moves towards it. \n they reach";
 	s += " out and touch it. \n 'look ";
 	s += getName();
-	s += " it's a portal!' \n You both walk through. \n";
+	s += " it's a portal!' \n 'This portal will lead you to an ancient city' the pheonix chirps, 'follow me, I'll show you around!' \n";
 	System.out.print(s);
 	City newYork = new City(__name, _treasure);
     }
