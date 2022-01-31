bb4e558c85aa0f0139f825cd31314c0610d3a87a
Joshua Gao <90897967+SpicyRamen247@users.noreply.github.com>
2022-01-31 02:30:31 -0500

    Update Woo.java

--- Woo.java
+++ Woo.java
@@ -1,28 +1,37 @@
-/** Watson vs The Final Project (Anjini Katari, Ruby Friedman, Joshua Gao)
-APCS
-FP
-2022-01-24
-total time spent: 20 hours
-*/
+
+/** Watson vs The Final Project
+Anjini Katari
+Ruby Friedman
+Joshua Gao */
 
 import java.io.*;
+//import java.util.*;
 import java.util.ArrayList;
 
-public class Woo {
-	private int choice;
-	private boolean gameOver;
-        public String s;
+
+private class Woo {
+    private int playerChoice;
+    private boolean gameOver;
+    public String prompt;
 	public String name = "";
-	private InputStreamReader isr;
-	private BufferedReader in;
-	public ArrayList treasure = new ArrayList<String>();
+    private InputStreamReader isr;
+	private BufferedReader br;
+    public ArrayList treasure = new ArrayList<String>();
+
+    public Woo() {
+		gameOver = false;
+		isr = new InputStreamReader( System.in );
+		br = new BufferedReader( isr );
+		playerChoice = 0;
+		newGame();
+	}
 
 	// ~~~~~~~~~~ DEFAULT CONSTRUCTOR ~~~~~~~~~~~
 	public Woo() {
 		gameOver = false;
-		isr = new InputStreamReader( System.in );
-		in = new BufferedReader( isr );
-		choice = 0;
+		isr = new InputStreamReader( System.br );
+		br = new BufferedReader( isr );
+		playerChoice = 0;
 		newGame();
 	}
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,11 +45,22 @@ public class Woo {
 		System.out.print( s );
 
 		try {
-			name = in.readLine();
+			name = br.readLine();
 		}
 		catch ( IOException e ) { }
 
-		s = "You and 3 of your closest friends are spending the weekend in a lovely log cabin in the woods, on a mountain. \n You wake up after your first night and find that all of your food has been eaten by a bear! \n Instead of driving back home, you all decide to try to find some food.\n  While on a hiking trail, you and your friends find some berries.\n One of your friends finds a group of birds eating them, and reasons that they would be safe to eat. \n That night you all snack on berries and other plants found in the forest. \n The next morning, you step outside the cabin for some fresh air, only for everything to look different from the day before. \n You've been teleported to a different universe!! \n \n Confused, you run back inside and wake up your friends. \n Together you collectively decide to explore the mountain, and split up into groups of 2. \n \n Which way would you like to explore? \n";
+		s = "
+        Hiking across the beautiful mountain on a Saturday afternoon, you and your 3 closest friend decided to spend the rest of the day relaxing at a lovely nearby log cabin
+        \n After a night of relaxful sleep, you eagerly go to awake your other friends before discovering a horrible truth: all your food has been stolen by a bear!
+        \n Knowing you still had 1 more day left, you and your friends collectively decided to camp the rest of the day and try to find some new food instead of driving back home.
+        \n Fortunately, during the hiking trail, you and you friends find some berries! Fantastic news! 
+        \n Knowing that the nearby wolf species eat these berries during times of winter, you and your friends reasoned they would be safe to eat. After all, you even see some local robins eating them this morning!
+        \n Lady Luck gave us a blessing. That night, we all had enough berries and other fruits found from the forest to eat. 
+        \n The following morning, you venture outside your cabin to inhale the incredible aroma of morning dew before realizing nothing looked the same. 
+        \n You've been teleported to a different universe!! 
+        \n Confused, you run back inside and wake up your friends to your new discovery. 
+        \n Astonished at this relevation, you and your friends decided to explore this new terrain as much as possible by splitting into teams of 2 to survey the surrounding.
+        \n Which way would you like to explore? \n";
 		System.out.println(s);
 		Cabin log  = new Cabin(name, treasure);
 	}
@@ -53,3 +73,11 @@ public class Woo {
 
 	}//end main
 } //end class woo
+
+
+
+
+
+
+
+
