42692c7a997f1ceb428bc1f0ad291c798145a4ae
Joshua Gao <90897967+SpicyRamen247@users.noreply.github.com>
2022-01-31 02:24:03 -0500

    Create BlackJack.java

---
+++ v6/BlackJack.java
@@ -0,0 +1,195 @@
+/** Watson vs The Final Project
+Anjini Katari
+Ruby Friedman
+Joshua Gao */
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class BlackJack {
+	protected int choice;
+	protected InputStreamReader isr;
+	protected BufferedReader in;
+	public boolean death;
+
+	public ArrayList deck= new ArrayList<Integer>();
+	public int YourNum = 0;
+	public int GobNum = 0;
+	public BlackJack() {
+		isr = new InputStreamReader( System.in );
+		in = new BufferedReader( isr );
+		choice = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 1; j <= 13; j++) {
+				deck.add(j);
+			}
+		}
+		blackjack();
+
+	}
+
+	public boolean getDeath() { return death;}
+	public void blackjack(){
+		System.out.print("Attempt to beat the Goblins by getting a count as close to 21 as possible, without going over 21!");
+		String s = "\n The goblin deals you a card...";
+		System.out.println(s);
+		Humhelper();
+		boolean var = true;
+		while (var) {
+			if (GobNum > 21) {
+				System.out.println("\n You win!");
+				var = false;
+				death = false;
+				break;
+			}
+			else if (GobNum == 21) {
+				System.out.println("\n The Goblin has won... death is your fate");
+				var = false;
+				death = true;
+				break;
+			}
+			else if(YourNum == 21) {
+                                System.out.println("\n You win!");
+				death = false;
+                                var = false;
+				break;
+                        }
+   	                else if (YourNum > 21) {
+                                System.out.println("\n The Goblin has won... death is your fate");
+				death = true;
+                                var = false;
+				break;
+                        }
+			else {
+                                if (GobNum <= 17) {
+                                        GobHelper();
+                                }
+        	       		System.out.print("\n Would you like to hit or pass? \n");
+
+	        	        s = "\t1: Hit \n";
+	     	          	s += "\t2: Pass \n";
+	        	        s += "Selection: ";
+
+	       		        System.out.println(s);
+
+	          	        try {
+	               			choice  = Integer.parseInt( in.readLine() );
+
+	               		}
+        		        catch ( IOException e ) { }
+
+	                        if (choice == 1) {
+			                Humhelper();
+	  	                }
+		                else {
+					if (GobNum <= 17) {
+						GobHelper();
+					}
+		                        else if (GobNum > 21) {
+        		                        System.out.println("\n You win!");
+      	        	                 	var = false;
+                        		        death = false;
+                        		        break;
+                       			}
+					else if (YourNum > GobNum) {
+	        	                        System.out.println("\n You win!");
+						death = false;
+        	        	                var = false;
+                		                break;
+					}
+					else if (GobNum > YourNum) {
+                	                	System.out.println("\n The Goblin has won... death is your fate");
+						death = true;
+                       		        	var = false;
+                       		        	break;
+					}
+	        	        }
+			}
+		}
+
+
+	}
+	
+	public String royal(int intInd, int index) {
+        String s = "";
+		if (intInd == 11){
+				s += "jack";
+			}
+		else if (intInd == 12){
+				s += "queen";
+			}
+		else if (intInd == 13){
+				s += "king";
+			}
+		else if (intInd == 1){
+				s += "ace";
+			}
+		else {
+			s += deck.get(index);
+		}
+        return s;
+	}
+		
+	public void Humhelper() {
+		String s = "\n It's a ";
+		int index = (int)(Math.random() * deck.size());
+        int intInd = (int)(deck.get(index));
+		YourNum += (int)(deck.get(index));
+
+        System.out.print(s);
+
+		if (index <= 13) {	
+            System.out.print(royal(intInd, index)); 
+			s = " of spades!";
+		}
+		
+		else if (index <= 26) {
+			System.out.print(royal(intInd, index)); 
+			s = " of hearts!";
+		}
+		else if (index <= 39) {}
+			System.out.print(royal(intInd, index)); 
+			s = " of diamonds!";
+		}
+		else {
+			System.out.print(royal(intInd, index)); 
+			s = " of clubs!";
+		}
+		s += "\n You now have ";
+		s += YourNum;
+		s+= " points";
+		System.out.print(s);
+		deck.remove(index);
+
+	}
+        public void GobHelper() {
+                String s = "\n The Goblin draws a card, it's a ";
+                int index = (int)(Math.random()* deck.size());
+				GobNum += (int) (deck.get(index));
+                if (index <= 13) {
+					System.out.print(royal(intInd, index));
+					s += " of spades!";
+				}
+				else if (index <= 26) {
+					System.out.print(royal(intInd, index));
+					s += " of hearts!";
+				}
+				else if (index <= 39) {
+					System.out.print(royal(intInd, index));
+					s += " of diamonds!";
+				}
+				else {
+					System.out.print(royal(intInd, index));
+					s += " of clubs!";
+				}
+                s += "\n The Goblin now has ";
+                s += GobNum;
+                s+= " points";
+                System.out.print(s);
+				deck.remove(index);
+
+        }
+
+
+
+}
